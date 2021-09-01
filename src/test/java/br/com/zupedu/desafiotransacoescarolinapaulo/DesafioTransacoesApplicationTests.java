e418f187bdde39dab96c228833931ce2c2869e96
CarolinaFaria <carolina.faria@zup.com.br>
2021-09-01 10:20:24 -0300

    setup projeto

---
+++ src/test/java/br/com/zupedu/desafiotransacoescarolinapaulo/DesafioTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupedu.desafiotransacoescarolinapaulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
